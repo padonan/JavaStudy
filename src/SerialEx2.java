77ff061720195e9dc9cd41b75611d24e72df3350
padonan <bbde1861@gmail.com>
2022-11-01 00:08:13 +0900

    1031 java base Study

---
+++ src/SerialEx2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class SerialEx2 {
+    public static void main(String[] args) {
+        try {
+            String fileName = "src/UserInfo.ser";
+            FileInputStream fis = new FileInputStream(fileName);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            ObjectInputStream in = new ObjectInputStream(bis);
+
+            UserInfo u1 = (UserInfo) in.readObject();
+            UserInfo u2 = (UserInfo) in.readObject();
+            ArrayList list = (ArrayList) in.readObject();
+
+
+            System.out.println(u1);
+            System.out.println(u2);
+            System.out.println(list);
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
